43796970a0989c6401b071496dea9b3efbc0a231
Ted Kerr <kerrtheodore13@gmail.com>
2023-04-24 12:32:11 -0400

    Added default preview for alpaca and css for customization buttons

--- alpacamaker/src/AlpacaGeneratorButtons/AlpacaGeneratorButtons.js
+++ alpacamaker/src/AlpacaGeneratorButtons/AlpacaGeneratorButtons.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from './AlpacaGeneratorButtons.css'
 
 function AlpacaGeneratorButtons() {
 	return (
@@ -6,11 +7,29 @@ function AlpacaGeneratorButtons() {
 			<div className="button-container">
 				<h3>Accessorize the Alpaca's</h3>
 				<div className="buttons">
+					<ul>
+						<li className="customize-button">Hair</li>
+						<li className="customize-button">Ears</li>
+						<li className="customize-button">Eyes</li>
+						<li className="customize-button">Mouth</li>
+						<li className="customize-button">Neck</li>
+						<li className="customize-button">Leg</li>
+						<li className="customize-button">Accessories</li>
+						<li className="customize-button">Background</li>
+					</ul>
 				</div>
 			</div>
 			<div className="button-container">
 				<h3>Style</h3>
 				<div className="buttons">
+					<ul>
+						<li className="customize-button">Default</li>
+						<li className="customize-button">Curls</li>
+						<li className="customize-button">Short</li>
+						<li className="customize-button">Bang</li>
+						<li className="customize-button">Elegant</li>
+						<li className="customize-button">Quiff</li>
+					</ul>
 				</div>
 			</div>
 		</div>
--- alpacamaker/src/AlpacaPreview/AlpacaPreview.js
+++ alpacamaker/src/AlpacaPreview/AlpacaPreview.js
@@ -1,18 +1,27 @@
 import React from "react";
-import blue50 from '../img/backgrounds/blue50.png'
+import blue50 from '../img/backgrounds/blue50.png';
+import neckDefault from '../img/neck/default.png';
+import nose from '../img/nose.png';
+import mouthDefault from '../img/mouth/default.png';
+import eyeDefault from '../img/eyes/default.png';
+import earDefault from '../img/ears/default.png';
+import accessoryDefault from '../img/accessories/headphone.png';
+import hairDefault from '../img/hair/default.png';
+import legDefault from '../img/leg/default.png';
+import styles from './AlpacaPreview.css'
 function AlpacaPreview() {
 
 	return (
 		<div id="preview-container">
 			<img id="background-image" src={blue50} alt="background"/>
-			<img id="neck-image" alt="neck" />
-			<img id="nose-image" alt="nose"/>
-			<img id="mouth-image" alt="mouth"/>
-			<img id="eye-image" alt="eye"/>
-			<img id="ear-image" alt="ear"/>
-			<img id="accessory-image" alt="accessory"/>
-			<img id="hair-image" alt="hair"/>
-			<img id="leg-image" alt="leg"/>
+			<img id="neck-image" src={neckDefault} alt="neck" />
+			<img id="nose-image" src={nose} alt="nose"/>
+			<img id="mouth-image" src={mouthDefault} alt="mouth"/>
+			<img id="ear-image" src={earDefault} alt="ear"/>
+			<img id="hair-image" src={hairDefault} alt="hair"/>
+			<img id="eye-image" src={eyeDefault} alt="eye"/>
+			<img id="leg-image" src={legDefault} alt="leg"/>
+			<img id="accessory-image" src={accessoryDefault} alt="accessory"/>
 		</div>
 	);
 }

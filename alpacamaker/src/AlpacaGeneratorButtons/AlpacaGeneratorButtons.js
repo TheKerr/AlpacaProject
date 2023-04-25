4442ceec7cfb787d4da265e57305b0aaa1a702fe
Ted Kerr <kerrtheodore13@gmail.com>
2023-04-25 12:39:36 -0400

    Added selecting options

--- alpacamaker/src/AlpacaGeneratorButtons/AlpacaGeneratorButtons.js
+++ alpacamaker/src/AlpacaGeneratorButtons/AlpacaGeneratorButtons.js
@@ -1,39 +1,75 @@
 import React from "react";
+import { useState } from "react";
 import styles from './AlpacaGeneratorButtons.css'
 
-function AlpacaGeneratorButtons() {
+function AlpacaGeneratorButtons(props) {
+
+	
+
+	const [curOptions, setCurOptions] = useState('hair');
+	const [selectedOptions, setSelectedOptions] = useState({
+		neck: 'default',
+		mouth: 'default',
+		leg: 'default',
+		hair: 'default',
+		eyes: 'default',
+		ears: 'default',
+		background: 'blue50',
+		accessory: 'default'
+	})
+
+	const categories = ['Hair', 'Ears', 'Eyes', 'Mouth', 'Neck', 'Leg', 'Accessories', 'Background'];
+	const selectedButtonClass = 'selected-button';
+
+	const categoryNameToOptions = (name) => {
+		switch(name) {
+			case 'Accessories':
+				return 'accessory';
+			default: 
+				return name.toLowerCase();
+		}
+	}
+	
+
 	return (
-		<div>
-			<div className="button-container">
-				<h3>Accessorize the Alpaca's</h3>
-				<div className="buttons">
-					<ul>
-						<li className="customize-button">Hair</li>
-						<li className="customize-button">Ears</li>
-						<li className="customize-button">Eyes</li>
-						<li className="customize-button">Mouth</li>
-						<li className="customize-button">Neck</li>
-						<li className="customize-button">Leg</li>
-						<li className="customize-button">Accessories</li>
-						<li className="customize-button">Background</li>
-					</ul>
-				</div>
-			</div>
-			<div className="button-container">
-				<h3>Style</h3>
-				<div className="buttons">
-					<ul>
-						<li className="customize-button">Default</li>
-						<li className="customize-button">Curls</li>
-						<li className="customize-button">Short</li>
-						<li className="customize-button">Bang</li>
-						<li className="customize-button">Elegant</li>
-						<li className="customize-button">Quiff</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	);
+    <div>
+      <div className="button-container">
+        <h3>Accessorize the Alpaca's</h3>
+        <div className="buttons">
+          <ul>
+            {categories.map((e) => (
+              <li
+                className={`customize-button ${
+                  curOptions === categoryNameToOptions(e)
+                    ? selectedButtonClass
+                    : ""
+                }`}
+				onClick={() => setCurOptions(categoryNameToOptions(e))}
+              >
+                {e}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="button-container">
+        <h3>Style</h3>
+        <div className="buttons">
+          <ul>
+            {props.options[curOptions].map((e) => (
+              <li
+                className={`customize-button ${
+                  selectedOptions[curOptions] === e ? selectedButtonClass : ""
+                }`}
+              >
+                {e}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AlpacaGeneratorButtons;
--- alpacamaker/src/App.js
+++ alpacamaker/src/App.js
@@ -4,14 +4,26 @@ import AlpacaPreview from './AlpacaPreview/AlpacaPreview';
 import AlpacaGeneratorButtons from './AlpacaGeneratorButtons/AlpacaGeneratorButtons';
 
 function App() {
+
+	const options ={
+		neck: ['backwards', 'forwards', 'default', 'thick'],
+		mouth: ['astonished', 'default', 'eating', 'laugh', 'tongue'],
+		leg: ['bubble tea', 'cookie', 'default', 'game console', 'backwards', 'forwards'],
+		hair: ['default', 'curls', 'bangs', 'elegant', 'fancy', 'quiff', 'short'],
+		eyes: ['angry', 'default', 'naughty', 'panda', 'smart', 'star'],
+		ears: ['default', 'backwards', 'forwards'],
+		background: ['blue50'],
+		accessory: ['earrings', 'flower', 'glasses', 'headphone'],
+	}
+
   return (
     <div className="App">
       <header className="App-header">
 		<h1>Alpaca Generator</h1>
       </header>
 	  <div id="generator-container">
-		<AlpacaPreview />
-		<AlpacaGeneratorButtons />
+		<AlpacaPreview options={options}/>
+		<AlpacaGeneratorButtons options={options} />
 		<div id="extra-buttons">
 			<div className="extra-button" id="randomize-button">🔀 Randomize</div>
 			<div className="extra-button" id="download-button">🖼 Download</div>
